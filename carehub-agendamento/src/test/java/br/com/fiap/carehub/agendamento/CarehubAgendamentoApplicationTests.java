1d08a20d0d5a6fd7cdda09a30cd9a92f19b09f32
Marcelo Augusto Ribas de Oliveira <margustoadm@hotmail.com>
2026-08-24 14:47:46 -0300

    fix: corrige package do teste de contexto

--- carehub-agendamento/src/test/java/br/com/fiap/carehub/agendamento/CarehubAgendamentoApplicationTests.java
+++ carehub-agendamento/src/test/java/br/com/fiap/carehub/agendamento/CarehubAgendamentoApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.fiap.carehub_agendamento;
+package br.com.fiap.carehub.agendamento;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
